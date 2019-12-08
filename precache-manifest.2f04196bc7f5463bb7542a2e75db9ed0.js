0a3f799bfe7226f2793fd66d277696ab81a95ae3
Raphaël Pinel <raphaelpinel@gmail.com>
2019-12-08 11:45:29 +0200

    Updates

---
+++ precache-manifest.2f04196bc7f5463bb7542a2e75db9ed0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6dc0bdcc7b364af8ca6cce088cbba50",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5a40d133b0c56c7b320a",
+    "url": "/monsters-rolodex/static/css/main.5699f955.chunk.css"
+  },
+  {
+    "revision": "7e894316149ac95bff0e",
+    "url": "/monsters-rolodex/static/js/2.994afdca.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.994afdca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5a40d133b0c56c7b320a",
+    "url": "/monsters-rolodex/static/js/main.a0d354bb.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
